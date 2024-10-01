d512001aac9b3ccda2cd564cb3aa07b89b04ef80
Brayan Hernández Cortés <brayannhc07@gmail.com>
2024-09-30 23:00:02 -0600

    Sección 11: Fin de la sección (Relaciones en TypeORM)

---
+++ 04-teslo-shop/src/seed/seed.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@nestjs/common';
+import { ProductsService } from '../products/products.service';
+import { initialData } from "./data/seed-data";
+
+@Injectable()
+export class SeedService {
+  constructor(private readonly productsService: ProductsService) {}
+
+  async executeSeed() {
+    await this.insertNewProducts();
+    return 'This action adds a new seed';
+  }
+
+  private async insertNewProducts() {
+    await this.productsService.deleteAllProducts();
+
+    const products = initialData.products;
+
+    const insertPromises = [];
+    products.forEach((product) => {
+      insertPromises.push(this.productsService.create(product));
+    });
+
+    await Promise.all(insertPromises);
+  }
+}
